0b14537d06e5fdb5e1062c2ae9c5b59f40673a35
Matheus Henrique da Costa Oliveira <matheushenriqueoli@hotmail.com>
2018-05-10 19:03:56 -0300

    criamos a classe usuario

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$tete = new Usuario();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$tete->loadbyID(3);
 
-echo json_encode($usuarios);
+echo $tete;
 
- ?>
+?>
