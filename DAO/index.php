da2869579e9d051ef2993a0cb48e31336652422b
Matheus Henrique da Costa Oliveira <matheushenriqueoli@hotmail.com>
2018-05-10 19:24:10 -0300

    atualização da classe usuario

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
+
+/*
+//Carrega somente um usuario
 $tete = new Usuario();
-
 $tete->loadbyID(3);
-
 echo $tete;
+*/
+/*
+//Carrega Uma lista de Usuários
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/*
+//Carrega uma lista de Usuários buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@-sd");
+echo $usuario;
 
 ?>
